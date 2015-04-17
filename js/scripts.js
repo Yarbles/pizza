3e3168e2fbd2d1b3b25160bb5cd7e4ea27e5c8c0
Reid <reidbaldwin@Reids-MacBook-Pro.local>
2015-04-17 14:23:12 -0700

    pass tests for large and pepperoni pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
 var Pizza = {
   price: 7,
+  pepperoni: false,
+  large: false,
+
+  pepperoniPlease: function() {
+    this.pepperoni = true;
+  },
+
+  largePlease: function() {
+    this.large = true;
+  },
 
 getPrice: function() {
+  if (this.pepperoni === true) {
+    this.price += 3;
+  }
+  if (this.large === true) {
+    this.price += 4;
+  }
   return this.price;
   }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,23 @@
 describe("Pizza", function() {
-    it("establishes the base price of a regular cheese pizza as $7", function() {
-      var testPizza = Object.create(Pizza);
-      testPizza.getPrice();
-      expect(testPizza.getPrice()).to.equal(7);
+  describe("pepperoniPlease", function() {
+    describe("largePlease", function() {
+      it("establishes the base price of a regular cheese pizza as $7", function() {
+        var testPizza = Object.create(Pizza);
+        testPizza.getPrice();
+        expect(testPizza.getPrice()).to.equal(7);
+      });
+
+      it("establishes the base price of a pepperoni pizza as $10", function () {
+        var testPizza = Object.create(Pizza);
+        testPizza.pepperoniPlease();
+        expect(testPizza.getPrice()).to.equal(10);
+      });
+
+      it("establishes the base price of a large pizza as $11", function () {
+        var testPizza = Object.create(Pizza);
+        testPizza.largePlease();
+        expect(testPizza.getPrice()).to.equal(11);
+      });
     });
   });
+});
